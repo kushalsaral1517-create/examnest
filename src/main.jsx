3c3c22bf4ffc99e15df7d5a3cb97a008b36c947a
kushalsaral1517-create <kushalsaral1517@gmail.com>
2026-04-27 12:32:07 +0530

    Set up React application entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
 import { useState, useEffect } from "react";
 
 const exams = [
